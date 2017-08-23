313590020484890512292d4ab94210ba35ee25a3
toure <tourekadiy@gmail.com>
2017-08-23 10:31:17 +0200

    modification .gitignore + program pour écrire et lister fichier => script.js;

---
+++ script.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+fs.writeFile('toto.txt', 'Hello Toto!', function(err) {
+    if (err) {
+        console.error(err);
+    }
+    console.log('toto.txt has been created')
+
+});
+fs.readdir('.', function(err, files) {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(files);
+});
